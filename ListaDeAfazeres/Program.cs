7bd3793c518a721c9e76e591ba550553aea092ad
sheyla lopes <sheyla.l.santos@aluno.senai.br>
2024-09-09 17:01:39 -0300

    teste

--- ListaDeAfazeres/Program.cs
+++ ListaDeAfazeres/Program.cs
@@ -3,8 +3,9 @@ using Microsoft.Extensions.Configuration;
 using System;
 using System.IO;
 using System.Data.SqlClient;
+using Org.BouncyCastle.Security;
 
-class Program
+class Programa
 {
     private static string connectionString;
 
@@ -39,3 +40,46 @@ class Program
         Console.WriteLine("Tarefa inserida com sucesso.");
     }
 }
+        Private static void menu()
+
+           Console.Clear(); // Limpa a tela para uma nova exibição de menu
+           Console.WriteLine("Menu:");
+           Console.WriteLine("1. Inserir String de Conexão");
+           Console.WriteLine("2. Exibir String de Conexão");
+           Console.WriteLine("3. Sair");
+           Console.Write("Escolha uma opção: ");
+
+
+var choice = Console.ReadLine();
+
+switch (choice)
+{
+    case "1":
+        InsertConnectionString();
+        break;
+    case "2":
+        DisplayConnectionString();
+        break;
+    case "3":
+        Console.WriteLine("Saindo...");
+        return; // Sai do método Main, encerrando o programa
+    default:
+        Console.WriteLine("Opção inválida. Tente novamente.");
+        break;
+}
+
+    {        
+     static void main()
+
+             int counter = 0;
+
+        // O loop 'while' continuará enquanto 'counter' for menor que 5
+        while (counter < 5)
+        {
+            Console.WriteLine($"Contador: {counter}");
+            counter++; // Incrementa o contador
+        }
+
+        Console.WriteLine("Loop concluído!");
+   }
+       
